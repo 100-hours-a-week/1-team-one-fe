3d265a23507ac7f07cb7e613473e807819ff3393
jieun0824 <suspen064@gmail.com>
2026-01-21 23:05:57 +0900

    feat: 회원가입 페이지에 유효성 검사 타입 추가

--- apps/web/src/pages/signup/ui/SignupPage.tsx
+++ apps/web/src/pages/signup/ui/SignupPage.tsx
@@ -1,8 +1,13 @@
+import z from 'zod';
+
 import { SignupForm } from '@/src/features/auth/signup';
 import { signup } from '@/src/features/auth/signup/api';
+import { signupSchema } from '@/src/features/auth/signup/model/signup-schema';
+
+type SignupValues = z.infer<typeof signupSchema>;
 
 export function SignupPage() {
-  const handleSubmit = async (values) => {
+  const handleSubmit = async (values: SignupValues) => {
     await signup({
       email: values.email,
       nickname: values.nickname,
