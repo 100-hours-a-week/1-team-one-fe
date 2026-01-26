0bcb5ca952a088a19c8dfd89e67fd4e64925f49e
jieun0824 <suspen064@gmail.com>
2026-01-26 21:02:04 +0900

    delete: 쓰지 않는 onboarding 페이지 삭제

--- apps/web/src/pages/onboarding/ui/OnboardingCharacterPage.tsx
+++
@@ -1,8 +0,0 @@
-export function OnboardingCharacterPage() {
-  return (
-    <div>
-      <h1>Onboarding - Character Select Page</h1>
-      <p>Route: /onboarding/character</p>
-    </div>
-  );
-}
--- apps/web/src/pages/onboarding/ui/OnboardingSurveyPage.tsx
+++
@@ -1,8 +0,0 @@
-export function OnboardingSurveyPage() {
-  return (
-    <div>
-      <h1>Onboarding - Survey Page</h1>
-      <p>Route: /onboarding/survey</p>
-    </div>
-  );
-}
