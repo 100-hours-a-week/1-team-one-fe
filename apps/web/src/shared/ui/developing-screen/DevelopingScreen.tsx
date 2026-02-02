d917a0ac8dab7457481c6d1dac55bc435db7e7cf
jieun0824 <suspen064@gmail.com>
2026-02-02 09:55:31 +0900

    fix(image): 개발중 화면 Image 경로 잘못된 것 추정

--- apps/web/src/shared/ui/developing-screen/DevelopingScreen.tsx
+++ apps/web/src/shared/ui/developing-screen/DevelopingScreen.tsx
@@ -4,7 +4,7 @@ export function DevelopingScreen() {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-4 px-4 opacity-40">
       <h2 className="text-text text-center text-2xl font-semibold">현재 개발 중인 화면입니다</h2>
-      <Image src={'/developing.png'} width={150} height={300} alt="Developing Screen" />
+      <Image src={'/images/main/developing.png'} width={150} height={300} alt="Developing Screen" />
       <p className="text-text-muted max-w-md text-center">
         해당 화면은 아직 개발 중에 있어 일부 기능이 제한될 수 있습니다
       </p>
