7ee0fe46873e6d7a8d8f7d8a4a004aa0fea751eb
jieun0824 <suspen064@gmail.com>
2026-02-13 00:42:49 +0900

    refactor(features/dnd): dnd 기능을 feature 슬라이스로 분리

---
+++ apps/web/src/features/dnd/config/index.ts
@@ -0,0 +1,5 @@
+export type { DndOptionIdType } from './constants';
+export { DND_DURATION_MINUTES, DND_OPTION_IDS } from './constants';
+export { DND_INFINITE_YEARS } from './constants';
+export { DND_MESSAGES } from './messages';
+export { DND_OPTIONS } from './options';
