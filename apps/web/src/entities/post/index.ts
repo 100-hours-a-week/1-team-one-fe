984a676d78bc6628b2c032334dd7820134e0882f
jieun0824 <suspen064@gmail.com>
2026-02-19 20:56:00 +0900

    feat: entities 에 post detail 타입 정의 추가

--- apps/web/src/entities/post/index.ts
+++ apps/web/src/entities/post/index.ts
@@ -1,9 +1,11 @@
 export type { PostCreateDataType, PostCreateRequestDTO } from './api/dto/post-create.dto';
+export type { PostAuthor, PostDetailData, PostTag } from './api/dto/post-detail.dto';
 export type {
   PostImageUploadUrlDataType,
   PostImageUploadUrlRequestDTO,
 } from './api/dto/post-image-upload.dto';
 export { createPostFn } from './api/post-create';
+export { fetchPostDetailFn } from './api/post-detail';
 export {
   POST_IMAGE_UPLOAD_ERROR_CODE,
   requestPostImageUploadUrlFn,
